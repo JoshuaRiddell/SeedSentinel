4a5bb246051fe60c97f96f111741abfa1816e721
Joshua Riddell <jridd30@gmail.com>
2020-10-13 13:40:41 +1000

    Add radio support

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,12 +2,20 @@
 #include "light.h"
 #include "DHT.h"
 #include <ArduinoJson.h>
+#include <RF24.h>
 
 Moisture moisture;
 Light light;
 DHT dht(4, DHT11);
+RF24 radio(9, 8);
+
+const byte address[6] = "pi001";
+//const uint64_t address = 0x7069303031;
 
 void setup() {
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);
+  
   Serial.begin(115200);
 
   moisture.setPowerPin(2);
@@ -19,13 +27,18 @@ void setup() {
   light.setup();
 
   dht.begin();
+
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.stopListening();
 }
 
 void loop() {
-  waitForSerialAvailable();
-  if (getDataCommandReceived()) {
+  //waitForSerialAvailable();
+  //if (getDataCommandReceived()) {
     sendJsonData();
-  }
+    delay(1000);
+  //}
 }
 
 void waitForSerialAvailable() {
@@ -53,4 +66,7 @@ void sendJsonAsString(StaticJsonDocument<1024> &doc) {
   String output;
   serializeJson(doc, output);
   Serial.println(output);
+
+  char text[] = "Hello world";
+  radio.write(text, sizeof(text));
 }
